1f95d641df1d22997956723124b52bfea892c7aa
petros <pe.p@ed.ac.uk>
2014-10-06 18:19:16 +0100

    Trying to make json stream work through HTTP. Also tried a websocket serv/client example with autobahn

--- server/twitterauth.py
+++ server/twitterauth.py
@@ -7,8 +7,11 @@ config['secret'] = "h404KjmXnx1IqO0ohUXy4WqpUHSJfMnJfAUzGSxkSiyG73IuUJ"
 
 from twitter import *
 import datetime
+import json
  
-ts = TwitterStream(
+class TwitterAuth():
+    def getStream(self):
+        ts = TwitterStream(
                     auth=OAuth(
                         config['oauth_token'],
                         config['oauth_token_secret'],
@@ -17,9 +20,16 @@ ts = TwitterStream(
                         )
                     )
 #openstream = ts.statuses.filter(track=words)
-openstream = ts.statuses.sample()
-for item in openstream:
-    print item
+        openstream = ts.statuses.sample()
+        return openstream
+
+if __name__ == '__main__':
+    auth = TwitterAuth()
+    openstream = auth.getStream()
+    for item in openstream:
+        tweet = json.dumps(item)
+        print tweet
+
 #    print item['user']['screen_name'], datetime.strptime(item['created_at'],'%a %b %d %H:%M:%S +0000 %Y'), item['text']
 #    if datetime.now() > stop:
 #        print datetime.now().isoformat()
